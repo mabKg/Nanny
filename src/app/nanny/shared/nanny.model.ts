d4b549c169c708d2002f8e6dcc606471d0cd1980
Mabatho Morebodi <mabathomorebodi@Mabathos-MacBook-Air.local>
2020-10-03 13:18:17 +0200

    Add proxy configuration, adds rental details style improvement, add pipes

--- src/app/nanny/shared/nanny.model.ts
+++ src/app/nanny/shared/nanny.model.ts
@@ -1,5 +1,5 @@
 export class Nanny {
-id: string;
+_id: string;
 firstName: string;
 lastName: string;
 idNumber: string;
--- src/app/nanny/shared/nanny.service.ts
+++ src/app/nanny/shared/nanny.service.ts
@@ -1,132 +1,16 @@
 import { Injectable} from '@angular/core';
 import { Observable } from 'rxjs';
 import { Nanny } from './nanny.model';
+import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class NannyService {
-  private nannies: Nanny[] = [{
-    id: '1',
-    firstName: 'Lebogang' ,
-    lastName: 'Koloti',
-    idNumber: '08876776878768',
-    image: 'http://via.placeholder.com/350*250',
-    age: 25,
-    gender: 'Female',
-    cell: '08376473778',
-    email: 'mab@gmail.com',
-    city: 'johannesburg',
-    province: 'Gauteng',
-    hobbies: 'i like playing with kids',
-    type: 'stay In',
-    rate: 200,
-    health: 'Good',
-    church: 'Impact',
-    qualification: 'Child care and CPR',
-    criminalRecord: true,
-    criminalDescription: '',
-    yearsOfExperience: 3,
-    kidsAges: '1-2,3-4,5-6',
-    referenceName: 'mabatho',
-    referenceNumber: '0845677877',
-    createdAt: '02-04-2020'
-
-  },
-  {
-    id: '2',
-    firstName: 'Lebogang' ,
-    lastName: 'Koloti',
-    idNumber: '08876776878768',
-    image: 'http://via.placeholder.com/350*250',
-    age: 25,
-    gender: 'Female',
-    cell: '08376473778',
-    email: 'mab@gmail.com',
-    city: 'johannesburg',
-    province: 'Gauteng',
-    hobbies: 'i like playing with kids',
-    type: 'stay In',
-    rate: 200,
-    health: 'Good',
-    church: 'Impact',
-    qualification: 'Child care and CPR',
-    criminalRecord: true,
-    criminalDescription: '',
-    yearsOfExperience: 3,
-    kidsAges: '1-2,3-4,5-6',
-    referenceName: 'mabatho',
-    referenceNumber: '0845677877',
-    createdAt: '02-04-2020'
-
-  },
-  {
-    id: '3',
-    firstName: 'Lebogang' ,
-    lastName: 'Koloti',
-    idNumber: '08876776878768',
-    image: 'http://via.placeholder.com/350*250',
-    age: 25,
-    gender: 'Female',
-    cell: '08376473778',
-    email: 'mab@gmail.com',
-    city: 'johannesburg',
-    province: 'Gauteng',
-    hobbies: 'i like playing with kids',
-    type: 'stay In',
-    rate: 200,
-    health: 'Good',
-    church: 'Impact',
-    qualification: 'Child care and CPR',
-    criminalRecord: true,
-    criminalDescription: '',
-    yearsOfExperience: 3,
-    kidsAges: '1-2,3-4,5-6',
-    referenceName: 'mabatho',
-    referenceNumber: '0845677877',
-    createdAt: '02-04-2020'
-},
-  {
-    id: '4',
-    firstName: 'Lebogang' ,
-    lastName: 'Koloti',
-    idNumber: '08876776878768',
-    image: 'http://via.placeholder.com/350*250',
-    age: 25,
-    gender: 'Female',
-    cell: '08376473778',
-    email: 'mab@gmail.com',
-    city: 'johannesburg',
-    province: 'Gauteng',
-    hobbies: 'i like playing with kids',
-    type: 'stay In',
-    rate: 200,
-    health: 'Good',
-    church: 'Impact',
-    qualification: 'Child care and CPR',
-    criminalRecord: true,
-    criminalDescription: '',
-    yearsOfExperience: 3,
-    kidsAges: '1-2,3-4,5-6',
-    referenceName: 'mabatho',
-    referenceNumber: '0845677877',
-    createdAt: '02-04-2020'
-  }];
-
-public getNannyById(nannyId: string): Observable<Nanny> {
-  return new Observable<Nanny>((observer) => {
-    setTimeout(() => {
-      const foundNanny = this.nannies.find((nanny) =>{
-return nanny.id === nannyId;
-      });
-      observer.next(foundNanny);
-    }, 500);
-  });
+  
+  constructor(private http: HttpClient) {}
+public getNannyById(nannyId: string): Observable<any> {
+  return this.http.get('/api/v1/nannies/' + nannyId)
 }
 
-  public getNanny(): Observable<Nanny[]> {
-    return  new Observable<Nanny[]>((observer) => {
-
-setTimeout(() => {
-  observer.next(this.nannies);
-}, 1000);
-});
+  public getNanny(): Observable<any> {
+   return this.http.get('/api/v1/nannies')
 }
 }
