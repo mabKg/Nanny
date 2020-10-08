48978bf23a60178f1d4ab8f43e8919047ab764db
Mabatho Morebodi <mabathomorebodi@Mabathos-MacBook-Air.local>
2020-10-08 11:39:45 +0200

    design change

---
+++ src/app/common/header/search-nanny/search-nanny.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-nanny',
+  templateUrl: './search-nanny.component.html',
+  styleUrls: ['./search-nanny.component.css']
+})
+export class SearchNannyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
