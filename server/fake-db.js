b89f6aef6a1b02e1a855f616c790904022b7a8bc
Mabatho Morebodi <mabathomorebodi@Mabathos-MacBook-Air.local>
2020-11-03 12:59:47 +0200

    nanny-created

--- server/fake-db.js
+++ server/fake-db.js
@@ -1,124 +1,21 @@
 
 const Nanny = require('./models/nanny');
 const User = require('./models/user');
+const Booking = require('./models/booking');
+
+const fakeDbData = require('./data.json');
+
+
 class FakeDb {
     constructor() {
-        this.Nannies = [{
-        firstName: 'Lebogang' ,
-    lastName: 'Koloti',
-    idNumber: '0887677687876',
-    image: 'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
-    age: 25,
-    gender: 'Female',
-    cell: '08376473778',
-    email: 'mab@gmail.com',
-    city: 'johannesburg',
-    streetName: 'Commissioner',
-    province: 'Gauteng',
-    hobbies: 'i like playing with kids',
-    type: 'stay In',
-    rate: 2000,
-    health: 'Good',
-    church: 'Impact',
-    qualification: 'Child care and CPR',
-    criminalRecord: true,
-    criminalDescription: '',
-    yearsOfExperience: 3,
-    kidsAges: '1-2,3-4,5-6',
-    referenceName: 'mabatho',
-    referenceNumber: '0845677877',
-    createdAt: '02-04-2020'
-        },
-        {firstName: 'mamsi' ,
-        lastName: 'Koloti',
-        idNumber: '08876776878768',
-        image: 'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
-        age: 25,
-        gender: 'Female',
-        cell: '08376473778',
-        email: 'mab@gmail.com',
-        city: 'johannesburg',
-        streetName: 'Commissioner',
-        province: 'Gauteng',
-        hobbies: 'i like playing with kids',
-        type: 'stay In',
-        rate: 200.00,
-        health: 'Good',
-        church: 'Impact',
-        qualification: 'Child care and CPR',
-        criminalRecord: true,
-        criminalDescription: '',
-        yearsOfExperience: 3,
-        kidsAges: '1-2,3-4,5-6',
-        referenceName: 'mabatho',
-        referenceNumber: '0845677877',
-        createdAt: '02-04-2020'
-            },
-            {firstName: 'mamsi' ,
-            lastName: 'Koloti',
-            idNumber: '08876776878768',
-            image: 'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
-            age: 25,
-            gender: 'Female',
-            cell: '08376473778',
-            email: 'mabs@gmail.com',
-            city: 'johannesburg',
-            streetName: 'Commissioner',
-            province: 'Gauteng',
-            hobbies: 'i like playing with kids',
-            type: 'stay In',
-            rate: 200.00,
-            health: 'Good',
-            church: 'Impact',
-            qualification: 'Child care and CPR',
-            criminalRecord: true,
-            criminalDescription: '',
-            yearsOfExperience: 3,
-            kidsAges: '1-2,3-4,5-6',
-            referenceName: 'mabatho',
-            referenceNumber: '0845677877',
-            createdAt: '02-04-2020'
-                },
-                {firstName: 'mamsi' ,
-                lastName: 'Koloti',
-                idNumber: '08876776878768',
-                image: 'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
-                age: 25,
-                gender: 'Female',
-                cell: '08376473778',
-                email: 'thaps@gmail.com',
-                city: 'johannesburg',
-                streetName: 'Commissioner',
-                province: 'Gauteng',
-                hobbies: 'i like playing with kids',
-                type: 'stay In',
-                rate: 200.00,
-                health: 'Good',
-                church: 'Impact',
-                qualification: 'Child care and CPR',
-                criminalRecord: true,
-                criminalDescription: '',
-                yearsOfExperience: 3,
-                kidsAges: '1-2,3-4,5-6',
-                referenceName: 'mabatho',
-                referenceNumber: '0845677877',
-                createdAt: '02-04-2020'
-                    }];
-
-            this.users = [{
-                username: "Test User",
-                email: "test@gmail.com",
-                password: "testtest"
-            }, {
-                username: "mabs",
-                email: "mabs@gmail.com",
-                password: "mabs01"
-            }];
+        this.Nannies = fakeDbData.nannies;
+        this.users = fakeDbData.users;
     }
 
     async cleanDb(){
         await User.deleteMany({});
        await Nanny.deleteMany({});
+       await Booking.remove({});
        
     }
     pushDataToDb() {
--- server/routes/nannies.js
+++ server/routes/nannies.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Nanny = require('../models/nanny');
-
+const User = require('../models/user');
+const { normalizeErrors } = require('../helpers/mongoose');
 const UserCtrl = require('../controllers/user');
+const booking = require('../models/booking');
 
 router.get('/secret',UserCtrl.authMiddleware, function(req, res) {
     res.json({"secret": true});
 });
-
-router.get('', function(req, res) {
-    Nanny.find({})
-    .select('-bookings')
-    .exec(function(err, foundNanny) {
-
-        res.json(foundNanny);
-    });
-    });
-
 
 router.get('/:id', function(req, res) {
     const nannyId = req.params.id;
@@ -31,5 +23,44 @@ router.get('/:id', function(req, res) {
         res.json(foundNanny);
     });
 });
+
+router.post('', UserCtrl.authMiddleware, function(req, res) {
+    const { firstName, lastName, idNumber, image, age, gender, cell, email, city, streetName, province, hobbies, type, rate, health, church,
+        qualification, criminalRecord, criminalDescription, yearsOfExperience, kidsAges, referenceName, referenceNumber, createdAt } = req.body;
+    const user = res.locals.user;
+
+    const nanny = new Nanny({firstName, lastName, idNumber, image, age, gender, cell, email, city, streetName, province, hobbies, type, rate, health, church,
+        qualification, criminalRecord, criminalDescription, yearsOfExperience, kidsAges, referenceName, referenceNumber, createdAt});
+        nanny.user = user;
+
+        Nanny.create(nanny, function(err, newNanny) {
+            if (err) {
+                return res.status(422).send({errors: normalizeErrors(err.errors)});
+            }
+
+            User.update({_id:user.id}, { $push: {nanny: newNanny}}, function(){});
+
+            return res.json(newNanny);
+        })
+})
+
+router.get('', function(req, res) {
+    const city = req.query.city;
+    const query = city ? {city: city.toLowerCase()} : {};
+
+    Nanny.find(query)
+    .select('-bookings')
+    .exec(function(err, foundNanny) {
+        if (err) {
+            return res.status(422).send({errors: normalizeErrors(err.errors)});
+       }
+
+       if (city && foundNanny.length === 0) {
+        return res.status(422).send({errors: [{title: 'No Nanny Found', detail: `There are no Nannies for city ${city}`}]});  
+       }
+       return res.json(foundNanny);
+    });
+
+    });
 
 module.exports = router;
